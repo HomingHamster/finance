fab3ba2333eb43fe6456ef10fad638819561ce0d
Felix Farquharson <felix.farquharson@gmail.com>
2026-06-23 00:15:24 +0100

    5

--- app.py
+++ app.py
@@ -304,6 +304,8 @@ def homepage():
         .code-snippet {{
           background: var(--code-bg);
           border: 1px solid var(--line);
+          width: fit-content;
+          margin: 0.7em;
         }}
 
 
