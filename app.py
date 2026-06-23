13b9c67015c80235b869813e31c2c5ab0e263879
Felix Farquharson <felix.farquharson@gmail.com>
2026-06-23 03:51:22 +0100

    update wording.

--- app.py
+++ app.py
@@ -397,11 +397,18 @@ def homepage():
                  <li>Sign up for a "Trading API" account on <a href="https://alpaca.markets">Alpaca Markets</a>, and get your ID approved.</li>
                  <li>Log in.</li>
                  <li>Select Live Trading, not Paper Trading, from the top left.</li>
-                 <li>Deposit money, via Crypto or Bank Transfer. You will likely want ~$30 or more. You can add funds with the Funds & Wallet menu item which is on the left of the Alpaca website when in Live Trading mode.</li>
+                 <li>Deposit money, via Crypto or Bank Transfer. You will likely want ~$30 or more. You can add funds with the Funds & Wallet menu item which is on the left of the Alpaca website when in Live 
+                   Trading mode. Remember there are fees for deposits, trades, and withdrawals.</li>
                  <li>On the right hand side of the main dashboard page, halfway down, regenerate your API keys. They will be for Live Trading if you are in Live Trading mode, or Paper Trading, if you are in Paper Trading mode.</li>
                  <li>Copy the keys into the places in the script where it says to configure API_KEY and SECRET_KEY.</li>
                  <li>Check the PAPER setting is set to match your API keys. By default it is False for Live Trading mode (with real money).</li>
-                 <li>Run the script, and make sure it stays running. It will print a line every two minutes saying the market is closed when the market is closed.
+                 <li>Run the script, and make sure it stays running the enitre time that you want your portfolio rebalanced automatically according to these tickers. It will print a line every two minutes saying
+                    the market is closed when the market is closed. If you stop the script, you will need to decide if you want to manually liquidate your stocks to cash, otherwise they will remain invested in
+                    the companies that you were invested in when you stopped it.
+                 <li>When you get to the point of withdrawing your funds again, there are different lengths of waiting time for stock sales to settle, and then for withdrawals as well. Once you have stopped the script
+                    and manually liquidated the stock (using the little (X) button on the right hand side of the Position section of the Dashboard), it will usually sell when the market opens for the sale. It will 
+                    then take 24h to settle. After it has settled, you can either buy BTCUSD and action a Bitcoin withdrawal (which is sometimes immedate). Or withdraw to your bank account, which sometimes takes 
+                    three days. These time depend on the time you sell, and the currency used to deposit or withdraw.
                    <div class="code-snippet"><pre><code>python3 finance_script.py</pre></code></div></li>
                  <li>Done. Hopefully your balance will go up or down.</li>
               </ol></p>
